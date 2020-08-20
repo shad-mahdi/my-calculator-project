d5d00c46679c7e0aa61b4e2986b6a4511984a65c
shad <shadmahdi1@gmail.com>
2020-08-20 14:56:53 +0300

    added action types

--- home.js
+++ home.js
@@ -1,268 +1,46 @@
-
-let num1 = 0;
-
-let num2;
-
-let num3 = 0;
-
-let num4;
-
-let condition = false;
-
-let result;
-
-
-
-
-
-
-function addition(){
-
-condition = true;
-
-result = num2+= num4;
-
-console.log(result);
-}
-
-function subtraction()
-{
-
-condition = true;
-
-   result = num2-= num4;
-  console.log(result);
-}
-function multiplications()
-{
-
-condition = true;
-
-   result = num2*= num4;
-  console.log(result);
-}function division()
-{
-
-condition = true;
-
-   result = num2 / num4;
-  console.log(result);
-}
-function equalSign() {
- console.log(num4);
-
-}
-
-
-
-
-
-
-
-
-
-if (condition === false)
-{
-function key0()
-{
-   num1+= '0';
-
-    num2 = parseFloat(num1)
-   console.log(num2);
-}
-
-function key1()
-{
-   num1+= '1';
-
-    num2 = parseFloat(num1)
-   console.log('hey');
-}
-function key2()
-{
-   num1+= '2';
-
-   num2 = parseFloat(num1)
-   console.log(num2);
-}function key3()
-{
-   num1+= '3';
-
-   num2 = parseFloat(num1)
-   console.log(num2);
-}function key4()
-{
-   num1+= '4';
-
-    num2 = parseFloat(num1)
-   console.log(num2);
-}function key5()
-{
-   num1+= '5';
-
-    num2 = parseFloat(num1)
-   console.log(num2);
-}function key6()
-{
-   num1+= '6';
-   ;
-    num2 = parseFloat(num1)
-   console.log(num2);
-}function key7()
-{
-   num1+= '7';
-
-    num2 = parseFloat(num1)
-   console.log(num2);
-}function key8()
-{
-   num1+= '8';
-
-   num2 = parseFloat(num1)
-   console.log(num2);
-}function key9()
-{
-   num1+= '9';
-
-   num2 = parseFloat(num1)
-   console.log(num2);
-}
-function decimal()
-{
-   num1+= '.';
-
-    num2 = parseFloat(num1)
-   console.log(num2);
-}
-}
-else if (condition === true) {
-  function key00()
-  {
-     num3+= '0';
-
-      num4 = parseFloat(num3)
-     console.log(num4);
-  }
-  function key11()
-  {
-     num3+= '1';
-
-      num4 = parseFloat(num3)
-     console.log('hello');
-  }
-  function key22()
-  {
-     num3+= '2';
-
-     num4 = parseFloat(num3)
-     console.log(num4);
-  }function key33()
-  {
-     num3+= '3';
-
-     num4 = parseFloat(num3)
-     console.log(num4);
-  }function key44()
-  {
-     num3+= '4';
-
-      num4 = parseFloat(num3)
-     console.log(num4);
-  }function key55()
-  {
-     num3+= '5';
-
-      num4 = parseFloat(num3)
-     console.log(num4);
-  }function key66()
-  {
-     num3+= '6';
-     ;
-      num4 = parseFloat(num3)
-     console.log(num4);
-  }function key77()
-  {
-     num3+= '7';
-
-      num4 = parseFloat(num3)
-     console.log(num4);
-  }function key88()
-  {
-     num3+= '8';
-
-     num4 = parseFloat(num3)
-     console.log(num4);
-  }function key99()
-  {
-     num3+= '9';
-
-     num4 = parseFloat(num3)
-     console.log(num4);
-  }
-  function decimal2()
-  {
-     num3+= '.';
-     num4 = parseFloat(num3)
-    console.log(num4);
-  }
-}
-
-
-//addition,subtraction,division, multiplications
-
-
-/*
-function addition()
-{
-
-
-condition = true;
-
- result = num2+= num4;
-
-  console.log(result);
-}
-
-function subtraction()
-{
-
-condition = true;
-
-   result = num2-= num4;
-  console.log(result);
-}
-function multiplications()
-{
-
-condition = true;
-
-   result = num2*= num4;
-  console.log(result);
-}function division()
-{
-
-condition = true;
-
-   result = num2 / num4;
-  console.log(result);
-}
-function equalSign() {
- console.log(num4);
-
-}
-*/
-
 let numbers = {
   num1: null,
   num2: null
 }
 
-let execType = null;
+let actionType = null;
+let result = null;
 
-function assignNumber(event) {
-  if(numbers.num1 === null || execType === null) {
-    numbers.num1 = event.style.height;
+function assignNumber(numberButton) {
+  if(numbers.num1 === null || actionType === null) {
+     numbers.num1 = numberButton.value;
+  } else {
+     numbers.num2 = numberButton.value;
   }
 
-  console.log('our number is ' + numbers.num1);
+  console.log('number one is ' + numbers.num1);
+  console.log('number two is ' + numbers.num2);
+  console.log('actionType is ' + actionType);
+
 }
+function changeActionType(actionButton) {
+   actionType = actionButton.value;
+}
+
+function findResult() {
+   if (actionType === '/') {
+      //devide 
+      result = numbers.num1 / numbers.num2;
+   } else if(actionType==="*") {
+      //multiply
+      result = numbers.num1 * numbers.num2;
+   }else if (actionType === '+') {
+      //add
+      result = numbers.num1 + numbers.num2;
+
+   }else if (actionType === '-') {
+      //minus
+      result = numbers.num1 - numbers.num2;
+   }
+
+
+   console.log('the result is ' + result);
+}
+
+
+
